5082818386ae47f25d4c1a95cc48f7b8bd8cf64a
Piotr Gasior <pgasior94@gmail.com>
2022-01-02 21:04:02 +0100

    Disable sidetone on arctis 5

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let api = hidapi::HidApi::new().unwrap();
+    let device = api.open(4152, 4778).expect("Unable to open device");
+
+    let buff = vec![0x04, 0x40, 0x02, 0x12, 0xc6, 0x00, 0x00];
+    device.write(&buff).unwrap();
+
+    let buff = vec![0x06, 0x81, 0x43, 0x01, 0x23];
+    device.write(&buff).unwrap();
 }
